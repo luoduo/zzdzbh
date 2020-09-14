01aea2ede75a59754328225c387f7d454c1e1036
luoduo <luoduotang@163.com>
2020-09-14 14:07:27 +0800

    redis 配置修改

--- zzdzbh-common/src/main/java/com/zzhl/config/RedisConfig.java
+++ zzdzbh-common/src/main/java/com/zzhl/config/RedisConfig.java
@@ -1,12 +1,27 @@
 package com.zzhl.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.lang.Nullable;
+
+import org.springframework.cache.interceptor.KeyGenerator;
+import java.lang.reflect.Method;
 
 /**
  * 【描述：】
@@ -15,43 +30,99 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  * @Date: 2020/09/11/ 16:36
  */
 @Slf4j
+//@ConditionalOnMissingBean
 @Configuration
-public class RedisConfig {
-
+public class RedisConfig extends CachingConfigurerSupport {
+    /**
+     * redisTemplate 默认的序列化方式为 JdkSerializationRedisSerializer
+     * StringRedisTemplate 的默认序列化方式为 StringRedisSerializer
+     * 使用 fastJsonRedisSerializer 替换默认序列化方式
+     */
 //    @Bean
-//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-//        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-//        template.setConnectionFactory(factory);
-//        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(
-//                Object.class);
-//        ObjectMapper om = new ObjectMapper();
-//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-//        jackson2JsonRedisSerializer.setObjectMapper(om);
-//        template.setKeySerializer(jackson2JsonRedisSerializer);
-//        template.setValueSerializer(jackson2JsonRedisSerializer);
-//        template.setHashKeySerializer(jackson2JsonRedisSerializer);
-//        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-//        return template;
+//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory);
+//
+//        FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
+//
+//        // 设置值value的序列化方式
+//        redisTemplate.setValueSerializer(fastJsonRedisSerializer);
+//        redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
+//
+//        // 设置键key的序列化方式
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+//
+//        redisTemplate.afterPropertiesSet();
+//        return redisTemplate;
 //    }
 
-//    @Bean(name = "redisTemplate")
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory factory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(factory);
-        setRedisTemplate(redisTemplate);
-        redisTemplate.afterPropertiesSet();
-        return redisTemplate;
+
+
+    /**
+     * 自定义缓存key的生成策略。默认的生成策略是看不懂的(乱码内容)
+     * 通过Spring 的依赖注入特性进行自定义的配置注入并且此类是一个配置类可以更多程度的自定义配置
+     * @return
+     */
+    @Override
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
     }
 
-    public void setRedisTemplate(RedisTemplate redisTemplate) {
-        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
-        // 设置值（value）的序列化采用FastJsonRedisSerializer。
-        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-        // 设置键（key）的序列化采用StringRedisSerializer。
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.afterPropertiesSet();
+
+    /**
+     * 缓存配置管理器
+     */
+    @Bean
+    public CacheManager cacheManager(LettuceConnectionFactory factory) {
+        //以锁写入的方式创建RedisCacheWriter对象
+        RedisCacheWriter writer = RedisCacheWriter.lockingRedisCacheWriter(factory);
+        //创建默认缓存配置对象
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
+        RedisCacheManager cacheManager = new RedisCacheManager(writer, config);
+        return cacheManager;
+    }
+
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+
+        //om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance ,
+                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+
+//        om.activateDefaultTyping(PolymorphicTypeValidator, ObjectMapper.DefaultTyping)
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        // 在使用注解@Bean返回RedisTemplate的时候，同时配置hashKey与hashValue的序列化方式。
+        // key采用String的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+        // value 序 列 化 方 式 采 用 jackson
+        //使用它操作普通字符串，会出现Could not read JSON  template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setValueSerializer(stringRedisSerializer);
+        // hash的key也采用String的序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+        // hash的value序列化方式采用jackson
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
     }
 }
+
--- zzdzbh-web/src/main/java/com/zzhl/contorller/TestController.java
+++ zzdzbh-web/src/main/java/com/zzhl/contorller/TestController.java
@@ -1,9 +1,15 @@
 package com.zzhl.contorller;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
 
 /**
  * 【描述：】
@@ -14,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    private final RedisTemplate<String,String> redisTemplate;
+    private final RedisTemplate<String,Object> redisTemplate;
 
     public TestController(RedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
@@ -30,8 +36,22 @@ public class TestController {
     @GetMapping("/redis")
     public String redis(){
         redisTemplate.opsForValue().set("a_key","a_value");
-        String a_key = redisTemplate.opsForValue().get("a_key");
+        String a_key = (String)redisTemplate.opsForValue().get("a_key");
         return a_key;
+    }
+
+    @GetMapping("/redisObj")
+    public String redisObj(){
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("d", 1);
+        map.put("e", new BigDecimal(100));
+        map.put("f", LocalDateTime.now());
+        map.put("g", new Date());
+        map.forEach((key,value) -> {
+            redisTemplate.opsForHash().put("a_key_obj_hash",key, value);
+        });
+//        Object a_key = (Object)redisTemplate.opsForHash().get("a_key_obj");
+        return JSON.toJSONString(map);
     }
 
 }
